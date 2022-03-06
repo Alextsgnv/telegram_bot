6a4de300da8471511c7c99d946811d9a08156c27
Alextsgnv <100084362+Alextsgnv@users.noreply.github.com>
2022-03-06 19:34:29 +0300

    Update function.py

--- function.py
+++ function.py
@@ -13,32 +13,40 @@ russian_stopwords = stopwords.words("russian")
 
 PATH = __file__.replace("function.py","")
 
+
 def lower_text(text:str):
     return text.lower()
+
 
 def clean_text(text:str):
     punctuation = string.punctuation
     punctuation+="«»"
     return text.translate(str.maketrans('', '', punctuation))
+ 
     
 def del_mul_spaces(text:str):
     return re.sub(r"\s+"," ",text)
+
 
 def del_number(text:str):
     numbers = re.compile("[0-9]+")
     return re.sub(numbers,"",text)
 
+
 def del_other_chars(text:str):
     substr = re.compile(r"[a-zA-ZА-Яа-я]\.[a-zA-ZА-Яа-я]\.")
     return re.sub(substr,"",text)
+
 
 def del_stopwords(text:str):
     list_words = text.split(" ")
     result = [token for token in list_words if token not in russian_stopwords]
     return " ".join(result)
 
+
 def stemm_text(text:str, stemmer=stemmer):
     return " ".join((stemmer.stem(token) for token in text.split(" ")))
+
 
 def del_one_char(text:str):
     return " ".join([token for token in text.split(" ") if len(token)>1])
@@ -49,10 +57,12 @@ def cleaning_text(text:str):
     for function in functions:
         text = function(text)
     return text
+ 
     
 def setting_words(text:str):
     text = del_one_char(stemm_text(del_stopwords(text)))
     return text
+
 
 def predict(message):
   text = message
